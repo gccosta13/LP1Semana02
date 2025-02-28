fe2cc797127896dc2099d832f0d02ca54bb21207
gccosta13 <gccosta2005@gmail.com>
2025-02-28 09:40:58 +0000

    smooth

--- SmoothOperator/Program.cs
+++ SmoothOperator/Program.cs
@@ -6,7 +6,13 @@ namespace SmoothOperator
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            Console.WriteLine("Insira um número inteiro: ")
+            string str = Console.ReadLine();
+            sbyte n=sbyte.Parse(str);
+            n--;
+            Console.WriteLine(n);
+            n++;
+            Console.WriteLine(n);
         }
     }
 }
