173c1fc7af18869e0da4033f859de66080816ee2
gccosta13 <gccosta2005@gmail.com>
2025-02-28 10:02:10 +0000

    various

---
+++ VariousOperators/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace VariousOperators
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Insira um número inteto-não negativo");
+            string str=Console.ReadLine();
+            byte n = byte.Parse(str);
+            Console.WriteLine(n/2);
+            Console.WriteLine(n<<3);
+            Console.WriteLine(n^6);
+            Console.WriteLine(n>10);
+
+
+        }
+    }
+}
