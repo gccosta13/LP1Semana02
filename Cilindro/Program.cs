89c9515aa033471fc2da1da0208bccb349cdd04a
gccosta13 <gccosta2005@gmail.com>
2025-02-28 11:36:14 +0000

    ex6 primeira parte

--- Cilindro/Program.cs
+++ Cilindro/Program.cs
@@ -6,7 +6,20 @@ namespace Cilindro
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            Console.WriteLine("Insira a altura do cilindro: ");
+            string str1 = Console.ReadLine();
+            Console.WriteLine("Insira o raio do cilindro: ");
+            string str2 = Console.ReadLine();
+            
+            double r = double.Parse(str1);
+            double a = double.Parse(str2);
+            
+            double v = Math.PI*Math.Pow(r,r)*a;
+            double sa = 2*Math.PI*r*(r+a);
+
+            Console.WriteLine($"{v:f3}");
+            Console.WriteLine($"{sa:f3}"); 
+
         }
     }
 }
---
+++ ValoresEspeciais/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace ValoresEspeciais
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            byte byte1 =byte.MaxValue;
+            byte byte2=byte.MinValue;
+
+            sbyte sbyte1=sbyte.MaxValue;
+            sbyte sbyte2=sbyte.MinValue;
+
+            short short1= short.MaxValue;
+            short short2=short.MinValue;
+
+            ushort ushort1= ushort.MaxValue;
+            ushort ushort2= ushort.MinValue;
+
+            int i1= int.MaxValue;
+            int i2= int.MinValue;
+
+            uint uint1=uint.MaxValue;
+            uint uint2=uint.MinValue;
+
+            long lon1=long.MaxValue;
+            long lon2=long.MinValue;
+
+            ulong ulon1=ulong.MaxValue;
+            ulong ulon2=ulong.MinValue;
+
+            char char1=char.MaxValue;
+            char char2=char.MinValue;
+
+            float flon1=float.MaxValue;
+            float flon2=float.MinValue;
+
+            double dbyte1=double.MaxValue;
+            double dbyte2=double.MinValue;
+
+            decimal de1=decimal.MaxValue;
+            decimal de2=decimal.MinValue;
+
+            Console.WriteLine(byte1);
+            Console.WriteLine(byte2);
+            Console.WriteLine(sbyte1);
+            Console.WriteLine(sbyte2);
+            Console.WriteLine(short1);
+            Console.WriteLine(short2);
+            Console.WriteLine(ushort1);
+            Console.WriteLine(ushort2);
+            Console.WriteLine(i1);
+            Console.WriteLine(i2);
+            Console.WriteLine(uint1);
+            Console.WriteLine(uint2);
+            Console.WriteLine(lon1);
+            Console.WriteLine(lon2);
+            Console.WriteLine(ulon1);
+            Console.WriteLine(ulon2);
+            Console.WriteLine(char1);
+            Console.WriteLine(char2);
+            Console.WriteLine(flon1);
+            Console.WriteLine(flon2);
+            Console.WriteLine(dbyte1);
+            Console.WriteLine(dbyte2);
+            Console.WriteLine(de1);
+            Console.WriteLine(de2);
+            
+        }
+    }
+}
